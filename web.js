a675e03dfffeae7bd883df22891f3ddffd8c427c
Aliaksandr Sazonenka <aliaksandr.sazonenka@gmail.com>
2013-07-10 21:43:57 +0000

    Make web.js read index.html and render it.

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var indexFileContent = fs.readFileSync('index.html', {'encoding', 'utf8'});
+    var indexFileContent = fs.readFileSync('index.html', {'encoding': 'utf8'});
     response.send(indexFileContent);
 });
 
